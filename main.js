28e30425e7f093b5fa98b59fb98834a6ce8c0fd8
luyvannda <philips.joe329@gmail.com>
2023-08-20 09:59:57 +0700

    use condition to check for 11 digits number start with country code 1 or 10 digit number it met some criteria but not all

---
+++ main.js
@@ -0,0 +1,15 @@
+function telephoneCheck(str) {
+
+  let newStr = str.replace(/[^\d+]/g, "");
+  console.log(typeof newStr, newStr)
+
+  let num = parseInt(newStr, 10);
+
+  console.log(typeof num, num);
+
+  return (num > 1000000000 && num < 9999999999
+    || num > 10000000000 && num < 19999999999);
+}
+
+let result = telephoneCheck("555-555-5555");
+console.log(result);
