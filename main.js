a72b1fd3a10c7f5d2a71e4072b1f15f3f006bc3e
luyvannda <philips.joe329@gmail.com>
2023-08-20 11:02:19 +0700

    resend the code because last commit is not going through

--- main.js
+++ main.js
@@ -5,17 +5,31 @@ function telephoneCheck(str) {
   if (invalid.test(str)) {
     return false
   }
-
+  // Check if the input string has a balanced pair of parentheses around the area code
   let openParenthesis = 0;
   let closeParenthesis = 0;
-
+  let areaCode = false; // flag to indicate if the area code is found
   for (let i = 0; i < str.length; i++) {
     if (str[i] === "(") {
       openParenthesis++;
+      if (openParenthesis > 1) {
+        return false;
+      }
+      areaCode = true; // set the flag to true when the area code starts
     } else if (str[i] === ")") {
       closeParenthesis++;
       if (closeParenthesis > openParenthesis) {
         return false;
+      }
+      areaCode = false;
+    } else if (str[i] >= "0" && str[i] <= "9") {
+      if (areaCode) {
+        // check if the area code has exactly three digits
+        let digits = str.slice(i, i + 3); // get the next three characters from the string
+        if (digits.length !== 3 || /\D/.test(digits)) {
+          return false; // return false if the area code is not three digits or contains non-digit characters
+        }
+        i += 2; // skip the next two characters
       }
     }
   }
@@ -27,12 +41,10 @@ function telephoneCheck(str) {
   console.log(typeof newStr, newStr)
 
   let num = parseInt(newStr, 10);
-
   console.log(typeof num, num);
-
-  return (num > 1000000000 && num < 9999999999
-    || num > 10000000000 && num < 19999999999);
+  // Check if the number is a valid 10-digit number or a valid 11-digit number starting with 1
+  return (num > 1000000000 && num < 9999999999 || num > 10000000000 && num < 19999999999);
 }
 
-let result = telephoneCheck("555-555-5555");
+let result = telephoneCheck("(6054756961)");
 console.log(result);
